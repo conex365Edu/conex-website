ac8374fb5a93d6b2f2a44f075a96d06a73dea70e
Your Name <your@email.com>
2020-08-21 16:41:54 +0530

    New Version

--- routes/api/conet.js
+++ routes/api/conet.js
@@ -38,30 +38,6 @@ router.post("/conet", (req, res) => {
             res.setHeader("Content-Type", "application/json");
             res.status(200).json(user);
             console.log(user);
-
-            const username = process.env.user;
-            const password = process.env.password;
-            //Send the Mail to the the client
-            const mailTransporter = nodemailer.createTransport({
-              service: process.env.service,
-              auth: {
-                user: username,
-                pass: password,
-              },
-            });
-
-            const mailDetails = {
-              from: "paulprince24542@gmail.com",
-              to: user.Email,
-              subject: "Welcome to Conet Platform Provided by Conex365 ",
-              body: `Hi ${user.Email}
-              Welcome to Conet Platform...We are delighted to serve you as our top priority client.
-              Our support team will be in touch with you within 12hrs`,
-            };
-            mailTransporter.sendMail(mailDetails, function (err, data) {
-              if (err) throw err;
-              console.log("Email was send successfully" + data.response);
-            });
           }
         });
       } else {
@@ -78,7 +54,7 @@ router.post("/conet", (req, res) => {
 router.get("/conet", async (req, res) => {
   const filter = {};
   const conet = await conetModel.find(filter);
-  console.log(conet)
+  console.log(conet);
   res.json(conet);
 });
 
