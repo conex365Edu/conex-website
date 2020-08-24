02815ff96f03ac6643c36c0e22643897c9aaf6b2
Paul Prince <paulprince2454@gmail.com>
2020-08-24 19:40:09 +0530

    SSL Updates

--- server.js
+++ server.js
@@ -249,7 +249,7 @@ app.get(
   }
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
